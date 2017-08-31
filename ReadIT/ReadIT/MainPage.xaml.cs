b125a16ee26a0cee36b390733674a21778bdd34e
jykim31337 <jykim31337@gmail.com>
2017-08-31 13:27:59 +0900

    초기 커밋.

---
+++ ReadIT/ReadIT/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ReadIT
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoadData_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEditData_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
